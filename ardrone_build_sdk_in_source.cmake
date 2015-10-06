4ff7a25af820e2e1fb6659e60f4588e2b6ae7f2b
Victor Arribas <v.arribas.urjc@gmail.com>
2015-10-06 13:44:06 +0200

    add ardonelib in-source compilation

---
+++ ardrone_build_sdk_in_source.cmake
@@ -0,0 +1,56 @@
+cmake_minimum_required(VERSION 2.8)
+
+project(build_ardronelib)
+
+## Build ARDrone SDK here
+set (ARDRONE_SDK_FLAVOR "autonomy")
+
+include(ExternalProject)
+
+if (${ARDRONE_SDK_FLAVOR} STREQUAL "autonomy")
+	message("ardronelib version: AutonomyLabs lastest")
+	set(ARDRONE_SDK_NAME "ardronelib" )
+	set(ARDRONE_SDK ${CMAKE_BINARY_DIR}/src/${ARDRONE_SDK_NAME}/ARDroneLib)
+	set(ARDRONE_SDK_LIB_PATH ${CMAKE_BINARY_DIR}/lib/ardrone)
+
+	ExternalProject_Add(ardronelib
+		GIT_REPOSITORY git://github.com/AutonomyLab/ardronelib.git
+		GIT_TAG master
+		PREFIX ${CMAKE_BINARY_DIR}
+		CONFIGURE_COMMAND echo "No configure"
+		BUILD_COMMAND make
+		INSTALL_COMMAND make install INSTALL_PREFIX=${ARDRONE_SDK_LIB_PATH}
+		BUILD_IN_SOURCE 1
+	)
+else()
+	message("ardronelib version: Official 2.0.1 patched")
+	set(ARDRONE_SDK_NAME "ardronelib" )
+	set(ARDRONE_SDK ${CMAKE_BINARY_DIR}/src/${ARDRONE_SDK_NAME}/ARDroneLib)
+	set(ARDRONE_SDK_LIB_PATH ${CMAKE_BINARY_DIR}/lib/ardrone)
+
+	ExternalProject_Add(ardronelib
+		GIT_REPOSITORY git://github.com/AutonomyLab/ardronelib.git
+		GIT_TAG bdacd1cbd3fbc54263d29e6e2067265e5941d10e
+		PREFIX ${CMAKE_BINARY_DIR}
+		CONFIGURE_COMMAND echo "No configure"
+		BUILD_COMMAND make
+		INSTALL_COMMAND make install INSTALL_PREFIX=${ARDRONE_SDK_LIB_PATH}
+		BUILD_IN_SOURCE 1
+	)
+endif()
+
+
+## Include ARDrone into build toolchain
+message( "ARDrone SDK at ${ARDRONE_SDK}" )
+message( "ARDrone LIBS at ${ARDRONE_SDK_LIB_PATH}" )
+
+include_directories(
+    ${ARDRONE_SDK}
+    ${ARDRONE_SDK}/FFMPEG/Includes
+    ${ARDRONE_SDK}/Soft/Common
+    ${ARDRONE_SDK}/Soft/Lib
+    ${ARDRONE_SDK}/VP_SDK
+    ${ARDRONE_SDK}/VP_SDK/VP_Os/linux
+)
+link_directories(${ARDRONE_SDK_LIB_PATH})
+
